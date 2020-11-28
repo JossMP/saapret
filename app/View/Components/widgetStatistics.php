0ccd0263c913b8db355edcdd6309f880e15f729e
Josue Mazco <JossMP@gmail.com>
2020-11-28 03:13:07 -0500

    Modificado nombre archivo y clase

--- app/View/Components/widgetStatistics.php
+++ app/View/Components/widgetStatistics.php
@@ -5,10 +5,11 @@ namespace App\View\Components;
 use App\Models\Career;
 use Illuminate\View\Component;
 
-class widgetStatistics extends Component
+class WidgetStatistics extends Component
 {
     public $careers = null;
-    public $title = 'Carreras Profesionales';
+    public $title   = 'Carreras Profesionales';
+
     public function __construct($title = null)
     {
         $this->title = ($title != null) ? $title : $this->title;
